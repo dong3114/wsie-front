6e07868c5b92b0dea4c1c0ef57c0179765db359b
dong3114 <scv3114@gmail.com>
2025-09-18 13:16:22 +0900

    fix: file form

--- src/components/uploadforms/UploadForm.jsx
+++ src/components/uploadforms/UploadForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { analyzeImage } from "../../services/api";
+import { Utils } from "../../utils/api/FileAPI";
 
 function UploadForm({ setReport }) {
   const [file, setFile] = useState(null);
@@ -8,13 +8,14 @@ function UploadForm({ setReport }) {
     e.preventDefault();
     if (!file) return alert("이미지를 선택하세요!");
 
-    try {
-      const result = await analyzeImage(file);
-      setReport(result);
-    } catch (error) {
-      console.error("분석 실패:", error);
-      alert("분석 요청 중 오류가 발생했습니다.");
-    }
+    Utils.file_upload(file)
+      .then((result) => {
+        setReport(result);
+      })
+      .catch((err) => {
+        console.error("분석 실패", err);
+        alert("분석 요청 중 오류가 발생했습니다.");
+      });
   };
 
   return (
